3f11dde005fb1e97c33869885447f9ba5d626b63
A-Hyeon <71107963+f-exuan21@users.noreply.github.com>
2025-03-25 19:28:24 +0900

    refactor: record 로 변경

--- movie-api-module/src/main/java/com/app/movie/presentation/dto/MovieRequestDto.java
+++ movie-api-module/src/main/java/com/app/movie/presentation/dto/MovieRequestDto.java
@@ -1,44 +1,13 @@
 package com.app.movie.presentation.dto;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class MovieRequestDto {
+public record MovieRequestDto(String title, Set<String> genres) {
 
-    @Size(max=10)
-    private String title;
-    private Set<String> genres;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MovieRequestDto that = (MovieRequestDto) o;
-        return Objects.equals(title, that.title) && Objects.equals(genres, that.genres);
+    public MovieRequestDto() {
+        this(null, null);
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, genres);
-    }
-
-    @Override
-    public String toString() {
-        return "MovieRequestDto{" +
-                "title='" + title + '\'' +
-                ", genres=" + genres +
-                '}';
-    }
 }
