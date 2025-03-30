cbdde8d15052459a3925bbc24862232e9e4b6e60
f-exuan21 <f-exuan21@gmail.com>
2025-03-30 16:12:54 +0900

    feat: pessimistic lock 구현

--- movie-infrastructure-module/src/main/java/com/app/movie/repository/ShowtimeJpaRepository.java
+++ movie-infrastructure-module/src/main/java/com/app/movie/repository/ShowtimeJpaRepository.java
@@ -1,17 +1,21 @@
 package com.app.movie.repository;
 
 import com.app.movie.entity.ShowtimeEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface ShowtimeJpaRepository extends JpaRepository<ShowtimeEntity, Long> {
 
-    List<ShowtimeEntity> findByMovieId(Long movieId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<ShowtimeEntity> findById(Long movieId);
     List<ShowtimeEntity> findByMovie_ReleaseDateLessThanEqual(LocalDate date);
 
     @Query("SELECT s FROM ShowtimeEntity s " +
